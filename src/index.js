2b427a8935beeaf375495916746c4400a71a74d6
PREETI SARDAR <82765024+PretulikaSardaris@users.noreply.github.com>
2024-04-08 16:24:49 +0200

    new files added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "@firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBXpaTKg6oGaWm75dxU7gc353-yJowmIUw",
+    authDomain: "notes-app-42087.firebaseapp.com",
+    projectId: "notes-app-42087",
+    storageBucket: "notes-app-42087.appspot.com",
+    messagingSenderId: "376626180391",
+    appId: "1:376626180391:web:803012b2a1d2fcc30cb902"
+  };
+
+  initializeApp(firebaseConfig)
